722ccc0cf80972cb1628e0a03ad5f2c6ac3c35be
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-03-24 03:20:35 +0700

    map sudah keluar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,17 @@
 import React from 'react'
 import { VectorMap } from '@south-paw/react-vector-maps'
+import world from '../lib/world.json'
 
 function Header() {
   return (
     <header className="bg-blue-500">
       <h1>Header</h1>
-      <div className="h-screen bg-red-500">
+      <div className="">
         <VectorMap
-          id="map"
-          map="world_mill"
-          backgroundColor="red"
-          // layers={[
-          //   {
-          //     name: 'markers',
-          //     data: [
-          //       {
-          //         latLng: [41.9, 12.45],
-          //         name: 'Vatican City',
-          //       },
-          //       {
-          //         latLng: [43.73, 7.41],
-          //         name: 'Monaco',
-          //       },
-          //     ],
-          //   },
-          // ]}
+          {...world}
           checkedLayers={['nz-auk']}
           currentLayers={['nz-wgn']}
+          key="world"
         />
       </div>
     </header>
